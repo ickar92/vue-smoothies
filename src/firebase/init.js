9e8829421b56076e8b91f20c09ea81c845045708
Marie <ickar92@gmail.com>
2018-05-26 09:18:53 +0300

    init firebase

---
+++ src/firebase/init.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBy0Xf69VSOXTzU1lDnHA55J851qXr5xdk",
+  authDomain: "ninja-smoothies-1493f.firebaseapp.com",
+  databaseURL: "https://ninja-smoothies-1493f.firebaseio.com",
+  projectId: "ninja-smoothies-1493f",
+  storageBucket: "ninja-smoothies-1493f.appspot.com",
+  messagingSenderId: "744961135279"
+};
+firebase.initializeApp(config);
